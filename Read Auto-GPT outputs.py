08412af76f7ff551ab4051532e47af009ec27f47
Rodrigo <nobrega@gmail.com>
2023-05-01 09:16:23 +0800

    Update Read Auto-GPT outputs.py
    Converting outputs to markdown OK. Basic functions documentation

--- Read Auto-GPT outputs.py
+++ Read Auto-GPT outputs.py
@@ -23,12 +23,62 @@
 
 from pathlib import Path
 import pandas as pd
+from IPython.display import Markdown
 
 
 def read_output(input_file):
+    """
+    Reads and returns the contents of a file.
+
+    Parameters:
+    input_file (str): Path to the input file.
+
+    Returns:
+    str: Contents of the file.
+    """
     with open(input_file, "r") as f:
         result = f.read()
     return result
+
+
+keywords_list = ["NEXT ACTION", "CRITICISM", "PLAN", "REASONING", "THOUGHTS", "SYSTEM", "NEWS"]
+
+
+def replace_headers(text, keywords):
+    """
+    Replaces the headers in the given text with markdown headings.
+
+    Parameters:
+    text (str): Input text.
+    keywords (list): List of keywords to replace with markdown headings.
+
+    Returns:
+    str: Text with markdown headings instead of headers.
+    """
+    for keyword in keywords:
+        text = text.replace(f"{keyword}: ", f"# {keyword}\n")
+    return text
+
+
+def interpret_output(input_file, keywords):
+    """
+    Reads the output file, replaces headers with markdown headings and returns the formatted text.
+
+    Parameters:
+    input_file (str): Path to the input file.
+    keywords (list): List of keywords to replace with markdown headings.
+
+    Returns:
+    str: Formatted text with markdown headings.
+    """
+    markdown = ""
+    separator = "-=-=-=-=-=-=-= COMMAND AUTHORISED BY USER -=-=-=-=-=-=-= "
+    with open(input_file, "r") as f:
+        file_contents = f.read()
+    iterations_list = file_contents.split(separator)
+    markdown = "\n".join(iterations_list)
+    new_markdown = replace_headers(markdown, keywords)
+    return new_markdown
 
 
 directory = "/Users/rodrigo/Code/auto-gpt/Auto-GPT/auto_gpt_workspace/"
@@ -39,7 +89,10 @@ print(autogpt_output)
 
 # ## 2. Interpretation
 
-read_output(autogpt_output)
+read_output(autogpt_output)[:2000]
+
+# interpret_output(autogpt_output)
+Markdown(interpret_output(autogpt_output, keywords_list))
 
 
 
